7d52d910c2820f01df53278923bbe2ab6b62ec26
tannukannaojiya <tannukannaojiya@gmail.com>
2023-01-26 22:44:41 +0530

    addjs

--- App.js
+++ App.js
@@ -1,1 +1,19 @@
+const number=document.querySelectorAll('.code')
+
+number[0].focus()
+
+number.forEach((code, idx)=>{
+    code.addEventListener('keydown',(e)=>{
+        if(e.key>=0 && e.key<=9){
+            number[idx].value=''
+            setTimeout(()=>number[idx + 1].focus(),10)
+
+        }else if(e.key === 'Backspace'){
+            setTimeout(()=>number[idx-1].focus(),10)
+        }
+
+        
+    })
+})
+
 
